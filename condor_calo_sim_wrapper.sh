e813be9ee7911e3e52ff0a26fda16067d2a9079d
Ruggero Turra <giurrero@gmail.com>
2017-08-09 00:02:21 +0200

    new paths, new image

--- condor_calo_sim_wrapper.sh
+++ condor_calo_sim_wrapper.sh
@@ -2,28 +2,11 @@
 
 hostname
 
-cd EMCalo-PhD-2017
+mkdir EMCalo_build
+cd EMCalo_build
 
-pwd
-ls -l
-
-mkdir EMCalo-PhD-2017_build
-cd EMCalo-PhD-2017_build
-
-echo "creating Makefile"
-pwd
-ls -l
 cmake -DGeant4_DIR=/usr/local/geant4/lib/Geant4-10.0.4/ ../EMCalo
-
-echo "compiling"
-
 make
-
-echo "running"
 
 ./TestEm3 ../EMCalo/mac_files/my_run_samp.mac
 
-pwd
-ls -ltr
-
-#cp run_samp_em_10GeV.root ../../
